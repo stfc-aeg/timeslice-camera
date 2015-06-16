fb2941ac912c3c5b4eab60ed99cb96eca235326f
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2015-06-16 10:27:12 +0100

    Make use of tornado options parser to define default addresses, ports etc

--- control_server/control_server.py
+++ control_server/control_server.py
@@ -7,37 +7,40 @@ import logging
 
 import tornado.ioloop
 import tornado.options
+from tornado.options import options
 
 def main():
 
+    # Define configuration options and add to tornado option parser
+    tornado.options.define("mcast_group", default="224.1.1.1", help="Set camera control multicast group address")
+    tornado.options.define("mcast_port", default=5007, help="Set camera control multicast port")
+    tornado.options.define("http_addr", default="0.0.0.0", help="Set HTTP server address")
+    tornado.options.define("http_port", default=8888, help="Set HTTP server port")
+    tornado.options.define("ctrl_addr", default="0.0.0.0", help="Set camera control server address")
+    tornado.options.define("ctrl_port", default=8008, help="Set camera control server port")
+
+    # Parse the command line options
     tornado.options.parse_command_line()
 
     # Create a multicast client for communication with cameras
-    mcast_group = '224.1.1.1'
-    mcast_port  = 5007
-    control_mcast_client = app.control_mcast_client.ControlMcastClient(mcast_group, mcast_port)
+    control_mcast_client = app.control_mcast_client.ControlMcastClient(options.mcast_group, options.mcast_port)
 
-    logging.info("Camera control multicast client created on %s:%d..." % (mcast_group, mcast_port))
+    logging.info("Camera control multicast client created on %s:%d..." % (options.mcast_group, options.mcast_port))
 
     # Launch the camera controller
     camera_controller = app.camera_controller.CameraController(control_mcast_client)
 
-    # Launch the Web server app for the control UI
-    web_host = '0.0.0.0'
-    web_port = 8888
+    # Launch the HTTP server app for the control UI
     control_web_server = app.control_web_server.CameraWebServer(camera_controller)
-    control_web_server.listen(web_port, web_host)
+    control_web_server.listen(options.http_port, options.http_addr)
 
-    logging.info("Web server listening on %s:%d" % (web_host, web_port))
+    logging.info("Web server listening on %s:%d" % (options.http_addr, options.http_port))
 
     # Launch the control TCP server for receving data from the cameras
-    tcp_host = '0.0.0.0'
-    tcp_port = 8008
     control_tcp_server = app.control_tcp_server.ControlTcpServer(camera_controller)
-    control_tcp_server.listen(tcp_port, tcp_host)
+    control_tcp_server.listen(options.ctrl_port, options.ctrl_addr)
 
-    logging.info("Camera control server listening on %s:%d..." % (tcp_host, tcp_port))
-
+    logging.info("Camera control server listening on %s:%d..." % (options.ctrl_addr, options.ctrl_port))
 
     # Enter IO processing loop
     tornado.ioloop.IOLoop.instance().start()

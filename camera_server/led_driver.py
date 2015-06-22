338a57d2c71423d9872c55ea175750c2d9e7c1e3
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2015-06-22 15:22:12 +0100

    Added threaded LED blink functionality

--- camera_server/led_driver.py
+++ camera_server/led_driver.py
@@ -1,4 +1,6 @@
 import importlib
+import threading
+import time
 
 class LedDriver(object):
     
@@ -24,13 +26,21 @@ class LedDriver(object):
         else:
             self.gpio = None
             
-        self.colour = LedDriver.OFF  
+        self.colour = LedDriver.OFF
+        self.blink_event = threading.Event()
+        self.blink_thread = None
             
     def set_colour(self, colour=None):
+        
+        self.blink_event.set()
+        self._set_colour(colour)
+        
+    def _set_colour(self, colour):
  
         self.colour = colour
         
         if self.gpio == None:
+            print "LED colour is now {}".format(colour)
             return
  
         red = self.gpio.LOW
@@ -47,4 +57,38 @@ class LedDriver(object):
         self.gpio.output(LedDriver.RedLed, red)
         self.gpio.output(LedDriver.GreenLed, green)
             
+    
+    def blink(self, colour1=0, colour2=1, period=1.0, repeat=0):
         
+        self.blink_event.clear()
+        self.blink_thread = threading.Thread(target = self._blink_loop, args=(colour1, colour2, period, repeat))
+        self.blink_thread.start()
+        
+    def _blink_loop(self, colour1, colour2, period, repeat):
+        
+        current_colour = colour1
+        
+        while not self.blink_event.is_set():
+            self._set_colour(current_colour)
+            time.sleep(period)
+            current_colour = colour2 if current_colour == colour1 else colour1
+            if repeat > 0:
+                repeat = repeat - 1
+                if repeat == 0:
+                    break
+            
+if __name__ == '__main__':
+    
+    led = LedDriver(True)
+    
+    led.set_colour(0)
+    led.set_colour(1)
+    led.set_colour(2)
+    led.set_colour(3)
+    
+    led.blink(0, 1, 0.1, 10)
+    time.sleep(2)
+    
+    led.blink(2,3, 0.1, 0)
+    time.sleep(2)
+    led.set_colour(1)    

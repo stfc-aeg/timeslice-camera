15f81b23e8c5bb7bf15dc4a82cd6aa47dd51f348
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2015-06-22 15:23:07 +0100

    LED GPIO use requires camera_server to be run sudo

--- scripts/start_all.sh
+++ scripts/start_all.sh
@@ -10,6 +10,6 @@ end=${end:=8}
 for node in $(seq $start $end); do 
   node_name="cam-${node}"
   echo -n "$node_name : "
-  ssh pi@$node_name 'cd /home/pi/develop/projects/timeslice/camera_server; python camera_server.py >/dev/null 2>&1 &'
+  ssh pi@$node_name 'cd /home/pi/develop/projects/timeslice/camera_server; sudo python camera_server.py >/dev/null 2>&1 &'
 done
 

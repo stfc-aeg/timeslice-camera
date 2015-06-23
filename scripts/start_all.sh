cb9d3d1889582f539006392780033dbd62f4414c
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2015-06-23 16:36:30 +0100

    Startup script now passes arbitrary argument list to nodes

--- scripts/start_all.sh
+++ scripts/start_all.sh
@@ -6,10 +6,11 @@ end=${1}
 shift
 start=${start:=1}
 end=${end:=8}
+args=$*
 
 for node in $(seq $start $end); do 
   node_name="cam-${node}"
   echo -n "$node_name : "
-  ssh pi@$node_name 'cd /home/pi/develop/projects/timeslice/camera_server; sudo python camera_server.py --idoffset=108 >/dev/null 2>&1 &'
+  ssh pi@${node_name} cd /home/pi/develop/projects/timeslice/camera_server\; sudo python camera_server.py ${args} \>/dev/null 2\>\&1 \&
 done
 

e453a23bad8880711466e015d8a1a35bbc855a30
VKTB <viktor.bozhinov@stfc.ac.uk>
2019-02-08 14:15:38 +0000

    Display countdown once capture button is clicked

--- control_server/static/js/camera_control_simplified.js
+++ control_server/static/js/camera_control_simplified.js
@@ -1,4 +1,26 @@
-/* onClick capture button */
+// onClick capture button 
 $('#capture').click(function() {
-        $.post('/capture');
+    // displays countdown element
+    $('#mainSection').html('<div id="countdown">5</div>');
+
+    countdown_timer();
+
+    // $.post('/capture');
 });
+
+
+// 5 second countdown timer; countdown element is dynamically updated
+function countdown_timer()
+{
+    var timeleft = 4;
+    var intervalTime = setInterval(function() {
+        document.getElementById('countdown').innerHTML = timeleft;
+        timeleft -= 1;
+
+        // "GO!" dispalyed when countdown timer reaches 0
+        if(timeleft <0){
+            clearInterval(intervalTime);
+            document.getElementById('countdown').innerHTML = "GO!";
+        }
+    }, 1000);
+}

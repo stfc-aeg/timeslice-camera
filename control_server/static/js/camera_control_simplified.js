2dd1d2d8a1073c39e9a707097ed9ff6a34396e56
VKTB <viktor.bozhinov@stfc.ac.uk>
2019-02-14 15:49:10 +0000

    Display loader once the capture action is triggered

--- control_server/static/js/camera_control_simplified.js
+++ control_server/static/js/camera_control_simplified.js
@@ -1,6 +1,5 @@
-// onClick capture button 
+// 'countdown_timer' function called when capture button is clicked
 $('#capture').click(countdown_timer);
-    loader();
 
 // 5 second countdown timer; countdown element is dynamically updated
 function countdown_timer()
@@ -14,12 +13,16 @@ function countdown_timer()
         timeLeft -= 1;
 
         // 'GO!' dispalyed and 'capture' function called when countdown timer reaches 0
-        if(timeLeft <0){
+        if(timeLeft <0) {
             clearInterval(intervalTime);
             document.getElementById('countdown').innerHTML = "GO!";
 
             // 'capture' function is called 0.5 secs after 'GO!' is displayed
             setTimeout(capture, 500);
+            // 'loader' function called 0.5 secs after 'GO!' is displayed
+            setTimeout(loader, 500);
+            // 'poll_camera_state' function called when countdown timer reaches 0
+            poll_camera_state();
         }
     }, 1000);
 }
@@ -29,3 +32,23 @@ function capture()
 {
     $.post('/capture');
 }    
+
+// loading animation displayed
+function loader()
+{
+    if ($('#countdown').html() == "GO!") {
+        $('#mainSection').html('<div class="loader"></div>'+
+                               '<div id="loader-message"></div>');
+    }
+}
+
+// polls camera state info and dynamically updates the loader message element
+function poll_camera_state()
+{
+	$.getJSON("/camera_state", function(response) {	        
+        $('#loader-message').html(response.capture_status);
+    });
+
+    // function called every 0.250 secs
+    setTimeout(poll_camera_state, 250);
+}

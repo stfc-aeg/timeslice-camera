107704e5cb83eea7f232cc383e90e7e776d170a7
VKTB <viktor.bozhinov@stfc.ac.uk>
2019-02-25 12:41:40 +0000

    Display Access Code page when Save button clicked; Refactor code to fix issues with heading tags

--- control_server/static/js/camera_control_simplified.js
+++ control_server/static/js/camera_control_simplified.js
@@ -9,9 +9,9 @@ function renderIndexPage()
     $('#mainSection').addClass('h-100');
     // displays systems state element and 'Capture' button
     $('#mainSection').html('<div class="col-md-12 h-50">'+
-                           '<h4><span id="system-state" class="badge"></span></h4></div>'+
+                           '<span id="system-state" class="badge"></span></div>'+
                            '<div class="col-md-12">'+
-                           '<a href="#capture" class="btn btn-primary" id="captureButton">Capture</a></div>');
+                           '<a href="#capture" class="btn btn-primary" id="captureButton"><h4>Capture</h4></a></div>');
 
     // 'renderCountdownPage' function called when 'Capture' button is clicked
     $('#captureButton').click(renderCountdownPage);
@@ -67,11 +67,11 @@ function poll_camera_state()
         // dynamically updates the system state element
         if(response.system_state == 0) {
             $('#system-state').removeClass('badge-success').addClass('badge-danger');
-            $('#system-state').html('Not ready');
+            $('#system-state').html('<h3>Not ready</h3>');
             $('#captureButton').addClass('disabled');
         } else {
             $('#system-state').removeClass('badge-danger').addClass('badge-success');
-            $('#system-state').html('Ready');
+            $('#system-state').html('<h3>Ready</h3>');
             $('#captureButton').removeClass('disabled');
         }
 
@@ -91,9 +91,62 @@ function poll_camera_state()
 // displays 'Retake' & 'Save' buttons
 function renderRetakeSavePage()
 {
-    $('#mainSection').html('<a href="#capture" class="btn btn-primary" id="retakeButton">Retake</a>'+
-                           '<a href="#" class="btn btn-primary" id="saveButton">Save</a>');
+    $('#mainSection').html('<a href="#capture" class="btn btn-primary" id="retakeButton"><h4>Retake</h4></a>'+
+                           '<a href="#" class="btn btn-primary" id="saveButton"><h4>Save</h4></a>');
 
     // 'renderIndexPage' function called when 'Retake' button is clicked
     $('#retakeButton').click(renderIndexPage);
+
+    // 'renderAccessCodePage' function called when 'Save' button is clicked
+    $('#saveButton').click(renderAccessCodePage);
+}
+
+function renderAccessCodePage()
+{
+    // sets class height to 100%
+    $('#mainSection').addClass('h-100');
+    // displays systems state element and 'Capture' button
+    $('#mainSection').html('<div class="container-fluid h-100">'+
+                           '<div class="row text-center">'+
+                           '<div class="col-md-12"><h2>&nbsp;</h2><h2>&nbsp;</h2></div>'+
+                           '</div>'+
+                           '<div class="row text-center">'+
+                           '<div class="col-md-4"></div>'+
+                           '<div class="col-md-4">'+
+                           '<div><h2><b>Your access code is:</b></h2></div>'+
+                           '<span id="access-code" class="badge badge-success"><h3>6701</h3></span>'+
+                           '</div>'+
+                           '<div class="col-md-4">'+
+                           '</div>'+
+                           '</div>'+
+                           '<div class="row text-center">'+
+                           '<div class="col-md-12"><h2>&nbsp;</h2><h2>&nbsp;</h2></div>'+
+                           '</div>'+
+                           '<div class="row text-center">'+
+                           '<div class="col-md-3"></div>'+
+                           '<div class="col-md-6">'+
+                           '<h4>Please write your access code on the card provided to you and follow the instructions on the back of the card to get a copy of your video.</h4>'+
+                           '<h4>&nbsp;</h4>'+
+                           '<h4>Please click Done when you are finished writing your access code.</h4>'+
+                           '</div>'+
+                           '<div class="col-md-3"></div>'+
+                           '</div>'+
+                           '<div class="row text-center">'+
+                           '<div class="col-md-12">'+
+                           '<h2>&nbsp;</h2>'+
+                           '<h2>&nbsp;</h2>'+
+                           '</div>'+
+                           '</div>'+
+                           '<div class="row text-center">'+
+                           '<div class="col-md-12">'+
+                           '<a href="" class="btn btn-primary" id="done-Button"><h4>Done</h4></a>'+
+                           '</div>'+
+                           '</div>'+
+                           '<div class="row text-center">'+
+                           '<div class="col-md-12">'+
+                           '<h2>&nbsp;</h2>'+
+                           '<h2>&nbsp;</h2>'+
+                           '</div>'+
+                           '</div>'+
+                           '</div>')
 }

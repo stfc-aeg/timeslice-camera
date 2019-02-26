b961477d330bf8cb8df76aa1aefbebcb993fe03d
VKTB <viktor.bozhinov@stfc.ac.uk>
2019-02-26 09:43:45 +0000

    More code refactoring; applying feedback from Import This session into practice

--- control_server/static/js/camera_control_simplified.js
+++ control_server/static/js/camera_control_simplified.js
@@ -1,29 +1,24 @@
-poll_camera_state();
+pollCameraState();
 
-// 'renderIndexPage' function called when page is loaded
 $(document).ready(renderIndexPage);
 
-function renderIndexPage()
-{
-    // sets class height to 100%
-    $('#mainSection').addClass('h-100');
-    // displays systems state element and 'Capture' button
-    $('#mainSection').html('<div class="col-md-12 h-50">'+
-                           '<span id="system-state" class="badge"></span></div>'+
-                           '<div class="col-md-12">'+
-                           '<a href="#capture" class="btn btn-primary" id="captureButton"><h4>Capture</h4></a></div>');
+function renderIndexPage() {
+    // Sets class height to 100% to allow for vertical centering
+    $('#main-section').addClass('h-100');
 
-    // 'renderCountdownPage' function called when 'Capture' button is clicked
-    $('#captureButton').click(renderCountdownPage);
+    $('#main-section').html('<div class="col-md-12 h-50">'+
+                            '<span class="badge" id="system-state"></span></div>'+
+                            '<div class="col-md-12">'+
+                            '<a class="btn btn-primary" id="capture-button" href="#"><h4>Capture</h4></a></div>');
+
+    $('#capture-button').click(renderCountdownPage);
 }
 
-function renderCountdownPage()
-{   
-    // removes class height to allow countdown element to be centered
-    $('#mainSection').removeClass('h-100')
-
-    // displays the countdown element
-    $('#mainSection').html('<div id="countdown">5</div>');
+function renderCountdownPage() {   
+    // Removes class height to allow countdown element to be centered
+    $('#main-section').removeClass('h-100');
+    
+    $('#main-section').html('<div id="countdown">5</div>');
 
     // 5 second countdown timer; countdown element is dynamically updated
     var timeLeft = 4;
@@ -31,122 +26,104 @@ function renderCountdownPage()
         document.getElementById('countdown').innerHTML = timeLeft;
         timeLeft -= 1;
 
-        // 'GO!' dispayed when countdown timer reaches 0
-        if(timeLeft <0) {
+        if(timeLeft < 0) {
             clearInterval(intervalTime);
             document.getElementById('countdown').innerHTML = "GO!";
-
-            // 'capture' function called 0.5 secs after 'GO!' is displayed
             setTimeout(capture, 500);
-            // 'renderLoadingPage' function called 0.5 secs after 'GO!' is displayed
             setTimeout(renderLoadingPage, 500);
         }
     }, 1000);
 
-    // post request to trigger the capture action
-    function capture()
-    {
+    function capture() {
+        // Triggers capture action
         $.post('/capture');
     }   
 } 
 
-function renderLoadingPage()
-{
-    // displays the loading element once countdown element displays 'GO!'
+function renderLoadingPage() {
     if ($('#countdown').html() == "GO!") {
-        $('#mainSection').html('<div class="loader"></div>'+
-                               '<div id="loader-message"></div>');
+        $('#main-section').html('<div class="loader"></div>'+
+                                '<div id="loader-message"></div>');
     }
 }
 
-// polls camera state info
-function poll_camera_state()
-{
+function pollCameraState() {
+    // Checks the state of the cameras
 	$.getJSON("/camera_state", function(response) {	     
-        
-        // dynamically updates the system state element
+        // Dynamically updates the system state element
         if(response.system_state == 0) {
             $('#system-state').removeClass('badge-success').addClass('badge-danger');
             $('#system-state').html('<h3>Not ready</h3>');
-            $('#captureButton').addClass('disabled');
+            $('#capture-button').addClass('disabled');
         } else {
             $('#system-state').removeClass('badge-danger').addClass('badge-success');
             $('#system-state').html('<h3>Ready</h3>');
-            $('#captureButton').removeClass('disabled');
+            $('#capture-button').removeClass('disabled');
         }
 
-        // dynamically updates the loader message element
         $('#loader-message').html(response.capture_status);
 
-        // 'renderRetakeSavePage' function called if rendering completed
         if(response.render_status == 3) {
             renderRetakeSavePage();
         }
     });
 
-    // function called every 0.250 secs
-    setTimeout(poll_camera_state, 250);
+    setTimeout(pollCameraState, 250);
 }
 
-// displays 'Retake' & 'Save' buttons
-function renderRetakeSavePage()
-{
-    $('#mainSection').html('<a href="#capture" class="btn btn-primary" id="retakeButton"><h4>Retake</h4></a>'+
-                           '<a href="#" class="btn btn-primary" id="saveButton"><h4>Save</h4></a>');
+function renderRetakeSavePage() {
+    $('#main-section').html('<a class="btn btn-primary" id="retake-button" href="#"><h4>Retake</h4></a>'+
+                            '<a class="btn btn-primary" id="save-button" href="#"><h4>Save</h4></a>');
 
-    // 'renderIndexPage' function called when 'Retake' button is clicked
-    $('#retakeButton').click(renderIndexPage);
+    $('#retake-button').click(renderIndexPage);
 
-    // 'renderAccessCodePage' function called when 'Save' button is clicked
-    $('#saveButton').click(renderAccessCodePage);
+    $('#save-button').click(renderAccessCodePage);
 }
 
-function renderAccessCodePage()
-{
-    // sets class height to 100%
-    $('#mainSection').addClass('h-100');
-    // displays systems state element and 'Capture' button
-    $('#mainSection').html('<div class="container-fluid h-100">'+
-                           '<div class="row text-center">'+
-                           '<div class="col-md-12"><h2>&nbsp;</h2><h2>&nbsp;</h2></div>'+
-                           '</div>'+
-                           '<div class="row text-center">'+
-                           '<div class="col-md-4"></div>'+
-                           '<div class="col-md-4">'+
-                           '<div><h2><b>Your access code is:</b></h2></div>'+
-                           '<span id="access-code" class="badge badge-success"><h3>6701</h3></span>'+
-                           '</div>'+
-                           '<div class="col-md-4">'+
-                           '</div>'+
-                           '</div>'+
-                           '<div class="row text-center">'+
-                           '<div class="col-md-12"><h2>&nbsp;</h2><h2>&nbsp;</h2></div>'+
-                           '</div>'+
-                           '<div class="row text-center">'+
-                           '<div class="col-md-3"></div>'+
-                           '<div class="col-md-6">'+
-                           '<h4>Please write your access code on the card provided to you and follow the instructions on the back of the card to get a copy of your video.</h4>'+
-                           '<h4>&nbsp;</h4>'+
-                           '<h4>Please click Done when you are finished writing your access code.</h4>'+
-                           '</div>'+
-                           '<div class="col-md-3"></div>'+
-                           '</div>'+
-                           '<div class="row text-center">'+
-                           '<div class="col-md-12">'+
-                           '<h2>&nbsp;</h2>'+
-                           '<h2>&nbsp;</h2>'+
-                           '</div>'+
-                           '</div>'+
-                           '<div class="row text-center">'+
-                           '<div class="col-md-12">'+
-                           '<a href="" class="btn btn-primary" id="done-Button"><h4>Done</h4></a>'+
-                           '</div>'+
-                           '</div>'+
-                           '<div class="row text-center">'+
-                           '<div class="col-md-12">'+
-                           '<h2>&nbsp;</h2>'+
-                           '<h2>&nbsp;</h2>'+
-                           '</div>'+
-                           '</div>'+
-                           '</div>')
+function renderAccessCodePage() {
+    $('#main-section').addClass('h-100');
+
+    $('#main-section').html('<div class="container-fluid h-100">'+
+                            '<div class="row text-center">'+
+                            '<div class="col-md-12"><h2>&nbsp;</h2><h2>&nbsp;</h2></div>'+
+                            '</div>'+
+                            '<div class="row text-center">'+
+                            '<div class="col-md-4"></div>'+
+                            '<div class="col-md-4">'+
+                            '<div><h2><b>Your access code is:</b></h2></div>'+
+                            '<span class="badge badge-success" id="access-code"><h3>6701</h3></span>'+
+                            '</div>'+
+                            '<div class="col-md-4">'+
+                            '</div>'+
+                            '</div>'+
+                            '<div class="row text-center">'+
+                            '<div class="col-md-12"><h2>&nbsp;</h2><h2>&nbsp;</h2></div>'+
+                            '</div>'+
+                            '<div class="row text-center">'+
+                            '<div class="col-md-3"></div>'+
+                            '<div class="col-md-6">'+
+                            '<h4>Please write your access code on the card provided to you and follow the instructions on the back of the card to get a copy of your video.</h4>'+
+                            '<h4>&nbsp;</h4>'+
+                            '<h4>Please click Done when you are finished writing your access code.</h4>'+
+                            '</div>'+
+                            '<div class="col-md-3"></div>'+
+                            '</div>'+
+                            '<div class="row text-center">'+
+                            '<div class="col-md-12">'+
+                            '<h2>&nbsp;</h2>'+
+                            '<h2>&nbsp;</h2>'+
+                            '</div>'+
+                            '</div>'+
+                            '<div class="row text-center">'+
+                            '<div class="col-md-12">'+
+                            '<a class="btn btn-primary" id="done-button" href="#"><h4>Done</h4></a>'+
+                            '</div>'+
+                            '</div>'+
+                            '<div class="row text-center">'+
+                            '<div class="col-md-12">'+
+                            '<h2>&nbsp;</h2>'+
+                            '<h2>&nbsp;</h2>'+
+                            '</div>'+
+                            '</div>'+
+                            '</div>')
 }
